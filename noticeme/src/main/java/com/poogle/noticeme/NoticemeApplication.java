d537e3aab9d7a6f1ec770c9934936a7b414f5d13
suhyunsim <cnc1106@naver.com>
2021-09-16 23:06:59 +0900

    feat: 프로젝트 초기화
    Issue #2

---
+++ noticeme/src/main/java/com/poogle/noticeme/NoticemeApplication.java
@@ -0,0 +1,13 @@
+package com.poogle.noticeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticemeApplication.class, args);
+	}
+
+}
---
+++ noticeme/src/test/java/com/poogle/noticeme/NoticemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poogle.noticeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
